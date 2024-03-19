f8f6a48c965b1bcc744c94ad9c6d41cedc2a8ce4
liamelliot <liam.elliot@gmail.com>
2024-03-19 17:47:32 -0400

    app update

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ async function setup() {
     const handleSuccess = (stream) => {
     const source = context.createMediaStreamSource(stream);
     source.connect(device.node);
-}
-navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    }
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
     .then(handleSuccess);
 
     // Fetch the exported patcher
