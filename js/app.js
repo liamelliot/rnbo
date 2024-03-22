0ccfc32734bebe9b0c65027d1bc481f1ff9a1c0f
liamelliot <liam.elliot@gmail.com>
2024-03-22 14:53:05 -0400

    added params to p5 and mouse

--- js/app.js
+++ js/app.js
@@ -1,54 +1,71 @@
+let alreadySetup = false;
+let device;
+let ctx;
+let response;
+let patcher;
+
+
+
+//your p5 stuffs
 function setup() {
   createCanvas(400, 400);
+  initialSetup();
 }
 
 function draw() {
   background(220);
+  if(alreadySetup == true){
+    device.parametersById.get("myVal").value = parseFloat(mouseX)/width;
+    let inputAmp = device.parametersById.get("inputAmp").value
+    background(inputAmp * 255)
+  }
+
+}
+
+function mousePressed() {
+  print("hithere");
+ 
+    if (alreadySetup) return;
+    alreadySetup = true;
+    ctx.resume();
+  print("async");
+	console.log("made it to run!")
+
+  setupAudio();
+ 
+}
+
+async function initialSetup()
+{
+  ctx = new (window.AudioContext || window.webkitAudioContext)();
+  response = await fetch("export/patch.export.json");
+  patcher = await response.json();
+  
+  device = await RNBO.createDevice({
+    context: ctx,
+    patcher
+  });
+}
+async function setupAudio() 
+{
+  try {
+    // Access user's microphone
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    console.log(stream)
+    // Create Media Stream source
+    const source = ctx.createMediaStreamSource(stream);
+    console.log(source)
+    // Connect source to RNBO Device
+    source.connect(device.node);
+    
+    // Connect RNBO Device to destination output
+    device.node.connect(ctx.destination);
+    
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 
 
-(async () => {
-  
-  const ctx = new (window.AudioContext || window.webkitAudioContext)();
-  const response = await fetch("export/patch.export.json");
-  const patcher = await response.json();
-  let setup = false;
-  
-  const device = await RNBO.createDevice({
-    context: ctx,
-    patcher
-  });
-  
-  const run = async () => {
-    if (setup) return;
-    setup = true;
-    ctx.resume();
 
-	console.log("made it to run!")
-    try {
-	
-	
-		
-      // Access user's microphone
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-      console.log(stream)
-      // Create Media Stream source
-      const source = ctx.createMediaStreamSource(stream);
-      console.log(source)
-      // Connect source to RNBO Device
-      source.connect(device.node);
-      
-      // Connect RNBO Device to destination output
-      device.node.connect(ctx.destination);
-      
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-  const onGainChange = (e) => device.parametersById.get(e.currentTarget.name).normalizedValue = parseFloat(e.currentTarget.value);
-  
-  document.querySelector("#start").onclick = run;
-  document.querySelector("#gain").onchange = onGainChange;
-})();
