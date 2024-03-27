79fee347ab76ca0fe20877bf3c6318677fd3024d
DavidBorts <Davidaborts@gmail.com>
2024-03-27 00:21:09 -0400

    added audio-reactive visuals

--- js/app.js
+++ js/app.js
@@ -102,7 +102,7 @@ function draw() {
   let inputAmp = device.parametersById.get("inputAmp").value;
   image(img, windowWidth/2, windowHeight/2);
   img.resize(windowWidth/2,0);
-  img.resize(img.width*inputAmp,0);
+  img.resize(img.width*inputAmp*2,0);
 
   textAlign(CENTER, CENTER);
   textFont(inconsolata);
